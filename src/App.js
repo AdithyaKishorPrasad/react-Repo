eea289eedbf1c9d22301bfbea1572a0bd91ea6e5
AdithyaKishorPrasad <adithyaprasad86@gmail.com>
2022-03-02 17:13:53 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,46 +1,66 @@
+// import React from 'react';
+
+// const LAUNCHES_QUERY = `
+// {
+//   launchesPast(limit: 10) {
+//     id
+//     mission_name
+//   }
+// }
+// `;
+
+// function App() {
+//   const launches = useLaunches();
+
+//   return (
+//     <div className="App">
+//       <h1> SpaceX Launches </h1>
+
+//       <ul>
+//         {/* {JSON.stringify(launches, null, 2)} */}
+
+//         {launches.map((launch) => (
+//           <li key={launch.id}>{launch.mission_name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function useLaunches() {
+//   const [launches, setLaunches] = React.useState([]);
+
+//   React.useEffect(() => {
+//     fetch('https://api.spacex.land/graphql/', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify({ query: LAUNCHES_QUERY }),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => setLaunches(data.data.launchesPast));
+//   }, []);
+
+//   return launches;
+// }
+
+// export default App;
+
 import React from 'react';
+import ReactDom from 'react-dom';
 
-const LAUNCHES_QUERY = `
-{
-  launchesPast(limit: 10) {
-    id 
-    mission_name 
-  }
-}
-`;
-
-function App() {
-  const launches = useLaunches();
-
+function TestState() {
+  const [colour, setColour] = React.useState('red');
   return (
-    <div className="App">
-      <h1> SpaceX Launches </h1>
-
-      <ul>
-        {/* {JSON.stringify(launches, null, 2)} */}
-
-        {launches.map((launch) => (
-          <li key={launch.id}>{launch.mission_name}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h1>Colour is : {colour}! </h1>
+      <div class="col">
+        <button type="button" onClick={() => setColour('blue')}>
+          Blue
+        </button>
+      </div>
+    </>
   );
 }
 
-function useLaunches() {
-  const [launches, setLaunches] = React.useState([]);
-
-  React.useEffect(() => {
-    fetch('https://api.spacex.land/graphql/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ query: LAUNCHES_QUERY }),
-    })
-      .then((response) => response.json())
-      .then((data) => setLaunches(data.data.launchesPast));
-  }, []);
-
-  return launches;
-}
-
-export default App;
+export default TestState;
+// ReactDom.render(<TestState />,document.getElementById('root'));
